09fbe26c4461c381d074dcabc567e8359b861813
yvargasfwdcostarica-lab <yvargasfwdcostarica@gmail.com>
2026-03-16 13:23:34 -0600

    HEROYNAV

--- RAICES DEL GOLFO/src/pages/Inicio.jsx
+++ RAICES DEL GOLFO/src/pages/Inicio.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/INICIO/NAVBAR/Navbar'
 import Hero from '../components/INICIO/HERO/Hero'
+import Informacion from '../components/INICIO/INFORMACION/Informacion'
 
  function Inicio() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Informacion />
     </div>
 
   
