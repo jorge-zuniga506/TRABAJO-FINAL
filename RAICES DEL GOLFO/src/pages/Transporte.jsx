7b69af687abf5284aba4c56713d63b00f5ef0a48
yvargasfwdcostarica-lab <yvargasfwdcostarica@gmail.com>
2026-03-17 15:16:53 -0600

    Actualizaciónes

---
+++ RAICES DEL GOLFO/src/pages/Transporte.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navbar from '../components/INICIO/NAVBAR/Navbar';
+import Footer from '../components/INICIO/IMGULTIMA/ImgUltima';
+import TransporteComponent from '../components/TRANSPORTE/Transporte';
+import './AcercaDe.css'; // Reusing similar hero/layout styles if applicable, or we can add specific ones
+
+function Transporte() {
+  return (
+    <div className="transporte-page">
+      <Navbar />
+
+      <header className="transporte-hero" style={{ 
+        backgroundColor: '#2e7d32', 
+        padding: '100px 20px', 
+        textAlign: 'center', 
+        color: 'white',
+        marginTop: '70px'
+      }}>
+        <h1>Horarios y Transporte</h1>
+        <p>Planifica tu viaje a la hermosa Isla Venado.</p>
+      </header>
+
+      <main className="transporte-container">
+        <TransporteComponent />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Transporte;
--- RAICES DEL GOLFO/src/routes/Routing.jsx
+++ RAICES DEL GOLFO/src/routes/Routing.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Inicio from '../pages/Inicio'
 import Habitaciones from '../pages/Habitaciones'
+import AcercaDe from '../pages/AcercaDe'
+import Transporte from '../pages/Transporte'
 
 function Routing() {
   return (
@@ -9,6 +11,8 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/habitaciones" element={<Habitaciones />} />
+        <Route path="/acerca-de" element={<AcercaDe />} />
+        <Route path="/transporte" element={<Transporte />} />
        
       </Routes>
     </BrowserRouter>
