a74b7915bdd8976dc89ed1afbfec9ff115c16a14
yvargasfwdcostarica-lab <yvargasfwdcostarica@gmail.com>
2026-03-17 11:48:27 -0600

    PAGINA DE HABITACIONES

--- RAICES DEL GOLFO/src/routes/Routing.jsx
+++ RAICES DEL GOLFO/src/routes/Routing.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Inicio from '../pages/Inicio'
-
+import Habitaciones from '../pages/Habitaciones'
 
 function Routing() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/habitaciones" element={<Habitaciones />} />
+       
       </Routes>
     </BrowserRouter>
   );
