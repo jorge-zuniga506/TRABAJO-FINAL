4dc72d608d8557d950543ea0f64a2e648247d25d
sgordonfwdcostarica-cell <sgordonfwdcostarica@gmail.com>
2026-03-17 14:16:22 -0600

    CAMBIE EL NAVBAR DE TOUR

--- RAICES DEL GOLFO/src/routes/Routing.jsx
+++ RAICES DEL GOLFO/src/routes/Routing.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Inicio from '../pages/Inicio'
 import Tours from '../pages/Tours'
+import Login from '../pages/Login'
+import Registro from '../pages/Registro'
+import Habitaciones from '../pages/Habitaciones'
             
 
 function Routing() {
@@ -10,6 +13,9 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/tours" element={<Tours />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/habitaciones" element={<Habitaciones />} />
       </Routes>
     </BrowserRouter>
   );
