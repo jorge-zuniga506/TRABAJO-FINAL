8ff9bdcf99bd679b05bbc8c52965f3bcd660b6b9
sgordonfwdcostarica-cell <sgordonfwdcostarica@gmail.com>
2026-03-17 14:04:54 -0600

    PAGINA DE TOURS

--- RAICES DEL GOLFO/src/routes/Routing.jsx
+++ RAICES DEL GOLFO/src/routes/Routing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Inicio from '../pages/Inicio'
+import Tours from '../pages/Tours'
 
 
 function Routing() {
@@ -8,6 +9,7 @@ function Routing() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/tours" element={<Tours />} />
       </Routes>
     </BrowserRouter>
   );
