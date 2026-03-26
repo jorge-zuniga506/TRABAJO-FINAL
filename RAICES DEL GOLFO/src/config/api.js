8e3e9e2ed4af3ace3fa40ec6273dec65b290e268
yvargasfwdcostarica-lab <yvargasfwdcostarica@gmail.com>
2026-03-26 11:16:23 -0600

    Cambios

--- RAICES DEL GOLFO/src/config/api.js
+++ RAICES DEL GOLFO/src/config/api.js
@@ -1,6 +1,6 @@
 // src/config/api.js
 
-export const API_BASE_URL = 'http://localhost:3001';
+export const API_BASE_URL = 'http://localhost:3007';
 
 export const ENDPOINTS = {
     USERS: `${API_BASE_URL}/users`,
